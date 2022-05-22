bacb891bf98365514be7dc752e511872d778f9af
Amr Khaled <105147582+TheRealRedCoder@users.noreply.github.com>
2022-05-22 22:48:47 +0200

    Add files via upload

---
+++ PopCorn_Test.c
@@ -0,0 +1,17 @@
+#include <tm4c123gh6pm.h>
+#define SW1 0x10
+#define SW2 0x01
+#define SW3 
+
+void Popcorn(){
+
+	LCD4bits_Cmd(clear_display)
+	LCD_WriteString("Popcorn");
+    	delay(200);
+	if(SW2==0x01){
+
+	counter(0, 60 )
+		
+ 			}
+
+	     }
