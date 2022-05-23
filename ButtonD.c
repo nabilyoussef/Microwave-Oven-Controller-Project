363aa2bdcaab2901c454975d98ad13fb9f37237b
Mustafa Sayed Al-Said <101906607+MustafaSayedAl-Said@users.noreply.github.com>
2022-05-23 13:16:23 +0200

    Update ButtonD.c

--- ButtonD.c
+++ ButtonD.c
@@ -1,121 +1,157 @@
-#include "Io.h"
-#include "TM4C123GH6PM.h"
-#include "keybad.h"
-#include "delay.h"
-#include "lcd_functions.h"
-#define SW1 0x10
-#define SW2 0x01
-
-void ButtonD (){
-
-if (keypad_scan() == 'D'){
+void CookTime(void){
 	unsigned char KeypadInput, KeypadInput1, KeypadInput2, KeypadInput3;
-	int save, save1, save2, save3;
-	int minutes;
-	int seconds;
-	int x;
+	int minutes = 0, seconds = 0, x;
+	int i;
+	restart:LCD4bits_Cmd(clear_display);
+	delay(500);
 	LCD_WriteString("Cooking Time?");
-	delayMs(1000);
+	delay(500);
+	LCD4bits_Cmd(clear_display);
+	delay(500);
 	LCD_WriteString("00:00");
-	delayMs(500);
-	dec_cursor(1);
-	KeypadInput = keypad_scan();
+	
+	//
+	KeypadInput = keypad_scan();//2
 	x = KeypadInput - '0';
 	if (x >= 0 && x <= 3){
+			dec_cursor(1);
 	LCD4bits_Data(KeypadInput);
-	delayMs(500);
-	save = x;
-						}	
-	KeypadInput1 = keypad_scan();
+	delay(500);
+	minutes += x*10;	
+	//
+	KeypadInput1 = keypad_scan();//0 to9
 	x = KeypadInput1 - '0';
-	if (save == 3){
-											if (x == 0){
-																											delayMs(500);
-																											dec_cursor(2);
-																											LCD4bits_Data(KeypadInput);
-																											LCD4bits_Data(KeypadInput1);
-																											save1 = x;
-																										}	
-										}
-		else if (save == 0)
+	if (minutes == 30)
 		{
-			if (x >= 1  && x <= 9){
-				delayMs(500);
-				dec_cursor(2);
-				LCD4bits_Data(KeypadInput);
-				LCD4bits_Data(KeypadInput1);
-				save1 = x;
-																																	}
-		}	
-	else {
-		if (x >= 0 && x <= 9)
-		{
-			delayMs(500);
+		if (x == 0){
+			delay(500);
 			dec_cursor(2);
 			LCD4bits_Data(KeypadInput);
 			LCD4bits_Data(KeypadInput1);
-			save1 = x;
+			minutes += x;
+		}
+	else {
+			LCD4bits_Cmd(clear_display);
+			LCD_WriteString("ERR");
+			delay(2000); 
+			goto restart;
+			} 			
+		}
+
+		else if (minutes == 0)
+		{
+			if (x >= 0  && x <= 9){
+				delay(500);
+				dec_cursor(2);
+				LCD4bits_Data(KeypadInput);
+				LCD4bits_Data(KeypadInput1);
+				minutes += x;
+		}
+			else {
+						LCD4bits_Cmd(clear_display);
+						LCD_WriteString("ERR");
+						delay(2000); 
+						goto restart;
+			}
+	}	
+	else {
+		if (x >= 0 && x <= 9)
+		{
+			delay(500);
+			dec_cursor(2);
+			LCD4bits_Data(KeypadInput);
+			LCD4bits_Data(KeypadInput1);
+			minutes += x;
 		}
 		
 				}
 		KeypadInput2 = keypad_scan();
 		x = KeypadInput2 - '0';
-		if (save == 3) {
+		if (minutes == 30) {
 			if (x == 0) {
-				delayMs(500);
-				dec_cursor(3);
+				delay(500);
+				dec_cursor(4);
 				LCD4bits_Data(KeypadInput);
+				inc_cursor(1);
 				LCD4bits_Data(KeypadInput1);
 				LCD4bits_Data(KeypadInput2);
-				save2 = x;
+				seconds += x*10;
+			}
+			else {
+						 LCD4bits_Cmd(clear_display);
+						 LCD_WriteString("ERR");
+						 delay(2000); 
+				goto restart;
 			}
 		}
 													
-if (x >= 0 && x <= 5)
+else {
+if	(x >= 0 && x <= 5)
 		{
-	delayMs(500);
-	dec_cursor(3);
+	delay(500);
+	dec_cursor(4);
 	LCD4bits_Data(KeypadInput);
+	inc_cursor(1);			
 	LCD4bits_Data(KeypadInput1);
 	LCD4bits_Data(KeypadInput2);
-	save2 = x;
+	seconds += x*10;
 		}
+	}
 		
 		KeypadInput3 = keypad_scan();
 		x = KeypadInput3 - '0';
-		if (save == 3) {
+		if (minutes == 30) {
 			if (x == 0) {
-				delayMs(500);
-				dec_cursor(3);
+				delay(500);
+				dec_cursor(5);
 				LCD4bits_Data(KeypadInput);
 				LCD4bits_Data(KeypadInput1);
+				inc_cursor(1);
 				LCD4bits_Data(KeypadInput2);
 				LCD4bits_Data(KeypadInput3);
-				save3 = x;
+				seconds += x;
+			}
+			else {
+						LCD4bits_Cmd(clear_display);
+						LCD_WriteString("ERR");
+						delay(2000); 
+				goto restart;
 			}
 		}
-	  if (x >= 0 && x <= 9)
+
+	  else { 
+			if (x >= 0 && x <= 9)
 		{
-		  delayMs(500);
-		  dec_cursor(4);
+		  delay(500);
+		  dec_cursor(5);
 		  LCD4bits_Data(KeypadInput);
 		  LCD4bits_Data(KeypadInput1);
-		  LCD4bits_Data(KeypadInput2)
+			inc_cursor(1);
+		  LCD4bits_Data(KeypadInput2);
 		  LCD4bits_Data(KeypadInput3);
-		  save3 = x;
+		  seconds += x;
 		}
-		
-		if (GPIO_PORTF_DATA_R == SW1){
-																											LCD4bits_Cmd (clear_display);
-																									}
-		if (GPIO_PORTF_DATA_R == SW2){
-																											minutes = save * 10 + save1;
-																											seconds = save2 * 10 + save3;
-																											LCD_Timer (minutes ,seconds);
-																										}
-
-	
-	
-	
 	}
 }
+	else {
+			LCD4bits_Cmd(clear_display);
+		  LCD_WriteString("ERR");
+			delay(2000); 
+		goto restart;
+}
+		
+		delay(500);
+		LCD4bits_Cmd(clear_display);
+		i=pause();
+		if(i==2)
+		{
+			LCD_Timer(minutes, seconds);//LCD DISPLAY TIME
+		}
+		if(i==1)
+		{
+			minutes=0;
+			seconds=0;
+			goto restart;
+		}
+			
+}	
