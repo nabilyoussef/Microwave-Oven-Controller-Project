ff86a316b55cda009263f6cb6090b5920b41a59c
MarkNashaat19 <104918165+MarkNashaat19@users.noreply.github.com>
2022-05-23 22:41:21 +0200

    Delete delay.h

--- delay.h
+++
@@ -1,4 +0,0 @@
-void delayUs(int n);   														   //Delay in Micro Seconds
-void delayMs(int n);   														   //Delay in Milli Seconds
-void Systick_Wait_10ms(void);
-void delay(unsigned long time);
