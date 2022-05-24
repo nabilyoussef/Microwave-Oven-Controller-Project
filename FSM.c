eaf843a8fb6a72ecdc2a9032d167ef89b86b889a
MoAdelX <99405693+MoAdelX@users.noreply.github.com>
2022-05-24 13:04:38 +0200

    Final changes

--- FSM.c
+++ FSM.c
@@ -1,4 +1,7 @@
 #include <C:\Keil\Labware\tm4c123gh6pm.h>
+#define SW1 0x10
+#define SW2 0x01
+#define SW3 0x0111
 typedef enum
 {
 	IDLE,
@@ -13,8 +16,7 @@ int CookingDone; //Set when cooking is done
 void MicrowaveStateMachine(){
 	char keypadIn = keypad_scan();
 	int state = IDLE; //initial state
-	int pause = 0;
-	int cooking_time_in_minutes;
+	int minutes_reload;
 	while(1){
 		switch(state){
 			IDLE:
@@ -30,49 +32,49 @@ void MicrowaveStateMachine(){
 						if(keypadIn == 'D'){
 							state = DATA_ENTRY;
 						}
-						if((GPIO_PORTA_DATA_R & 0X08) == 0X00){
+			            if((GPIO_PORTB_DATA_R & 0X1111) == SW3){ e.g: on
 							state = IDLE;
 						}
 						break;
 			COOKS:
-						if(GPIO_PORTF_DATA_R == SW1){
+						if((GPIO_PORTF_DATA_R &0X11) == SW1){
 							state = PAUSE;
 						}
 						if(CookingDone == 1){
 							state = IDLE;
 						}
-						if((GPIO_PORTA_DATA_R & 0X08) == 0X00){
-						state = PAUSE;
-						break;
+						if((GPIO_PORTB_DATA_R & 0X1111) == SW3){ e.g: on
+							state = PAUSE;
 						}
+						break;
 			 DATA_ENTRY:
 					  if(keypadIn == 'B' | keypadIn == 'C'){
-							if(keypadIn <= 1 || keypadIn >= 9){
+						    keypadIn = (int)keypadIn - '0';
+							if((int)keypadIn <= 1 || (int)keypadIn >= 9){
 							state = DATA_ENTRY;
 							}
 						}
 						if(keypadIn == 'D'){
-							if(cooking_time_in_minutes<=1 || cooking_time_in_minutes >= 30){
-							state = DATA_ENTRY;}
-							else{
+							if(minutes_reload >= 30 && minutes_reload <=1)
+							state = DATA_ENTRY; 
+						else
 								state = COOKS;
-							}
 						}
-						if(keypadIn == 'B' && GPIO_PORTF_DATA_R == SW2){
+						if(keypadIn == 'B' && (GPIO_PORTF_DATA_R0x11) == SW2){
 						state = COOKS;
 						}
-						if(keypadIn == 'C' && GPIO_PORTF_DATA_R == SW2){
+						if(keypadIn == 'C' && (GPIO_PORTF_DATA_R&0x11) == SW2){
 						state = COOKS;
 						}
-						if(keypadIn == 'D' & GPIO_PORTF_DATA_R == SW2){
+						if(keypadIn == 'D' && (GPIO_PORTF_DATA_R&0x11) == SW2){
 						state = COOKS;
 						}
 						break;
 				PAUSE:
-						if(GPIO_PORTF_DATA_R == SW2 | &&  (GPIO_PORTA_DATA_R&0X08) ==  0X08){
+						if((GPIO_PORTF_DATA_R&0X11) == SW2 || (GPIO_PORTB_DATA_R&0X1111)== 0X1111){
 						state = COOKS;
 						}
-						if(GPIO_PORTF_DATA_R == SW1){
+						if((GPIO_PORTF_DATA_R&0X11) == SW1){
 						state = IDLE;
 				    }					 
 			}
